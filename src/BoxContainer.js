24e27d8ff7795885a8705d647f489ecec7e8aef9
Robbie <RobertIGottlieb@gmail.com>
2022-11-30 22:45:06 -0500

    completed front-end back-end integration

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -10,191 +10,83 @@ import SchemaBox from './schemaBox';
 
 // mock data for searched:
 // to test first update username in the state to any mock string as well
-const mockDataForSearch = [
-  {
-    code: `<!DOCTYPE html>
-    <html>
-    <head>
-        <title>
-            How to insert spaces/tabs in text using HTML/CSS?
-        </title>
-    </head>
-    <body>
-        <h1 style="color: green">GeeksforGeeks</h1>
-        <b>How to insert spaces/tabs in text using HTML/CSS?</b>
-
-        <p>This is a &nbsp; regular space.</p>
-        <p>This is a &ensp; two spaces gap.</p>
-        <p>This is a &emsp; four spaces gap.</p>
-    </body>
-    </html>`,
-    translation: 'This is a function with console.log',
-  },
-  {
-    code: 'useEffect(() => {setInputTextLength(inputText.toString().length)',
-    translation: 'this is another function',
-  },
-];
 
 function BoxContainer() {
   // updates current text in user input box
   const [inputText, setInputText] = useState('');
+
   // updates length of current input text
   const [inputTextLength, setInputTextLength] = useState(0);
+
   // displays text returned from server
   const [outputText, setOutputText] = useState('');
+
   // keeps track of the current username in the session
-  const [username, setUsername] = useState('Robbie');
+  const [username, setUsername] = useState('');
+
   // sets history menu to open or close
   const [open, setHistoryOpen] = useState(false);
+
   // fixes mui text field bug when label doesn't shrink after text is copied into input
   const [shrinkComponent, setShrinkComponent] = useState({});
+
   // keeps track of search history
-  const [searched, setSearched] = useState(mockDataForSearch);
+  const [searched, setSearched] = useState([
+    {
+      query: 'See query history here',
+      translation: 'See translation history here',
+    },
+  ]);
+
   // text displayed in input box
   const [inputLabel, setInputLabel] = useState('Paste your code');
+
   // keeps track of currently selected query method
   const [queryMode, setQueryMode] = useState('code-to-en');
+
   // text displayed in output box
   const [outputLabel, setOutputLabel] = useState('Plain English');
+
   // changes text in copy button
   const [expButtonText, setExpButtonText] = useState('COPY EXPLANATION');
+
   // changes text of input box placeholder
   const [inputBoxPlaceholder, setInputBoxPlaceholder] = useState('');
+
   // keeps track of user inputted schema
   const [userSchema, setUserSchema] = useState('');
 
+  //use effect hook checks if we have a username set in sessionStorage
+  useEffect(() => {
+    const activeUsername = sessionStorage.getItem('username');
+    if (activeUsername) {
+      setUsername(activeUsername);
+    }
+  }, [username]);
+
+  //use effect hook to display the length of the input text (cannot be over 250 characters)
   useEffect(() => {
     setInputTextLength(inputText.toString().length);
-  });
+  }, [inputTextLength]);
 
   // functionality to get previously researched queries from the database
   useEffect(() => {
     if (username.length !== 0) {
-      console.log('useEffect hook: ', username);
       const requestURI = `${process.env.BACKEND_USER_URI}/getRequests`;
+
+      //get request to backend for previous search history
       const getRequests = async () => {
         const response = await axios.post(requestURI, {
           username,
         });
-        console.log(response.data);
-        setSearched(response.data);
+        //if there are previous searches in the databases, set search in state to the
+        //array of previous searches that are returned from the backend
+        if (response.data.length > 0) setSearched(response.data);
       };
       getRequests();
     }
   }, [username]);
 
-  // function to invoke when user clicks one previously searched query
-  // we expect to see full code and translation in the input / output boxes
-
-  // function utilizes hooks to shrink the label component within the user input text field
-  // label within text field does not shrink when text is inputted programattically
-  // rather than manually typed
-  // use this method anywhere you are programatically copying and pasting into the user input field
-  function handleElementClick(obj) {
-    setShrinkComponent({ shrink: 'true' });
-    document.querySelector('#filled-multiline-static').value = obj.code;
-    // REMINDER: DOUBLECHECK THIS. To test you can click on an element in the history dropdown
-    setOutputText(obj.translation);
-  }
-
-  // adds whatever is in user input text field into state
-  // attached to event listener in component
-  const handleTyping = (event) => {
-    setInputText(event.target.value);
-  };
-
-  // copies the output box to user's clipboard
-  const CopyToClipBoardNormal = (event) => {
-    if (!outputText) return;
-
-    // functionality formats copied text with line break is created first space after 40 characters
-    const lineLength = 40;
-    let copyOutput = outputText[0];
-    let readyToCopy = false;
-    for (let i = 1; i < outputText.length; i++) {
-      if (i % lineLength === 0) readyToCopy = true;
-      if (readyToCopy && outputText[i] === ' ') {
-        copyOutput += '\n';
-        readyToCopy = false;
-        continue;
-      }
-      copyOutput += outputText[i];
-    }
-    // utilizes clipboard API to copy text to user's clipboard
-    navigator.clipboard.writeText(copyOutput);
-  };
-
-  // adds sudocode styling so user's can easily copy directly into code
-  const CopyToClipBoardSudo = (event) => {
-    if (!outputText) return;
-
-    // REMINDER:  condense this into one function that can also be used with copyclipboard normal
-    const lineLength = 40;
-    let copyOutput = outputText[0];
-    let readyToCopy = false;
-    for (let i = 1; i < outputText.length; i++) {
-      if (i % lineLength === 0) readyToCopy = true;
-      if (readyToCopy && outputText[i] === ' ') {
-        copyOutput += '\n';
-        readyToCopy = false;
-        continue;
-      }
-      copyOutput += outputText[i];
-    }
-    const textToCopy = `/*\n ${copyOutput} \n */`;
-    navigator.clipboard.writeText(textToCopy);
-  };
-
-  // sends contents of user input text field to backend
-  const handleSubmit = async (event) => {
-    // console.log(JSON.stringify(inputText));
-    event.preventDefault();
-    let requestURI;
-    if (queryMode === 'code-to-en') requestURI = `${process.env.BACKEND_API_URI}/entocode`;
-    if (queryMode === 'en-to-code') requestURI = `${process.env.BACKEND_API_URI}/codetoen`;
-    if (queryMode === 'en-to-sql') requestURI = `${process.env.BACKEND_API_URI}/entosql`;
-
-    // request body to be sent to backend
-    const json = {
-      query: inputText,
-    };
-
-    if (userSchema) {
-      json.schema = userSchema;
-    }
-
-    // sending username if user is logged in
-    if (username.length > 0) {
-      json.username = username;
-    }
-
-    // axios request to backend
-    const response = await axios.post(requestURI, json, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-    });
-    // sets output text element in state to equal what is returned from backend
-    setOutputText(response.data);
-  };
-
-  // handles opening of dropdown menu
-  const handleHistoryOpen = () => {
-    setHistoryOpen(!open);
-  };
-
-  // handling query mode changes -> setting mode
-  const handleCodeToEnClick = () => {
-    setQueryMode('code-to-en');
-  };
-  const handleEnToCodeClick = () => {
-    setQueryMode('en-to-code');
-  };
-  const handleEnToSQLClick = () => {
-    setQueryMode('en-to-sql');
-  };
   // handling updating of variables depending on query mode
   let schemaBox;
   useEffect(() => {
@@ -216,6 +108,131 @@ function BoxContainer() {
       setExpButtonText('COPY SQL QUERY');
     }
   }, [queryMode]);
+
+  // function to invoke when user clicks on previously searched query
+  function handleElementClick(historyel) {
+    // label within inputtext  does not shrink when text is inputted programattically
+    // rather than manually typed (this is a MUI bug)
+    // use setShrinkComponent anywhere you are programatically copying and pasting into the user input field
+    setShrinkComponent({ shrink: 'true' });
+
+    //set value (i.e. html text) of the inputBox to equal the query field of the history element
+    document.querySelector('#filled-multiline-static').value = historyel.query;
+
+    //set input text + output text + schema in state to be equal to the history element
+    setOutputText(historyel.translation);
+    setUserSchema(historyel.schema);
+    return;
+  }
+
+  // adds whatever is in user input text field into state
+  // attached to event listener in component
+  const handleTyping = (event) => {
+    setInputText(event.target.value);
+    return;
+  };
+
+  // copies the output box to user's clipboard
+  const CopyToClipBoardNormal = (event) => {
+    if (!outputText) return;
+
+    // functionality formats copied text with line break is created first space after 40 characters
+    const lineLength = 40;
+    let copyOutput = outputText[0];
+    let readyToCopy = false;
+    for (let i = 1; i < outputText.length; i++) {
+      if (i % lineLength === 0) readyToCopy = true;
+      if (readyToCopy && outputText[i] === ' ') {
+        copyOutput += '\n';
+        readyToCopy = false;
+        continue;
+      }
+      copyOutput += outputText[i];
+    }
+    // utilizes clipboard API to copy text to user's clipboard
+    navigator.clipboard.writeText(copyOutput);
+    return;
+  };
+
+  // adds sudocode styling so user's can easily copy directly into code
+  const CopyToClipBoardSudo = (event) => {
+    if (!outputText) return;
+
+    // REMINDER:  condense this into one function that can also be used with copyclipboard normal
+    const lineLength = 40;
+    let copyOutput = outputText[0];
+    let readyToCopy = false;
+    for (let i = 1; i < outputText.length; i++) {
+      if (i % lineLength === 0) readyToCopy = true;
+      if (readyToCopy && outputText[i] === ' ') {
+        copyOutput += '\n';
+        readyToCopy = false;
+        continue;
+      }
+      copyOutput += outputText[i];
+    }
+    const textToCopy = `/*\n ${copyOutput} \n */`;
+    navigator.clipboard.writeText(textToCopy);
+    return;
+  };
+
+  // sends contents of user input text field to backend
+  const handleSubmit = async (event) => {
+    // console.log(JSON.stringify(inputText));
+    event.preventDefault();
+    let requestURI;
+    if (queryMode === 'code-to-en')
+      requestURI = `${process.env.BACKEND_API_URI}/codetoen`;
+    if (queryMode === 'en-to-code')
+      requestURI = `${process.env.BACKEND_API_URI}/entocode`;
+    if (queryMode === 'en-to-sql')
+      requestURI = `${process.env.BACKEND_API_URI}/entosql`;
+
+    // request body to be sent to backend
+    const json = {
+      query: inputText,
+    };
+
+    if (userSchema) {
+      json.schema = userSchema;
+    }
+
+    // sending username if user is logged in
+    if (username.length > 0) {
+      json.username = username;
+    }
+
+    // axios request to backend
+    try {
+      const response = await axios.post(requestURI, json, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    // sets output text element in state to equal what is returned from backend
+    setOutputText(response.data.translation);
+    return;
+  };
+
+  // handles opening of dropdown menu
+  const handleHistoryOpen = () => {
+    setHistoryOpen(!open);
+  };
+
+  // Group of function for handling changing the mode of the query -> setting mode
+  const handleCodeToEnClick = () => {
+    setQueryMode('code-to-en');
+  };
+  const handleEnToCodeClick = () => {
+    setQueryMode('en-to-code');
+  };
+  const handleEnToSQLClick = () => {
+    setQueryMode('en-to-sql');
+  };
 
   // handles updating of user schema on submission of schema box
   const handleSchemaSubmit = (e) => {
@@ -255,19 +272,19 @@ function BoxContainer() {
         <ButtonGroup variant='outlined' aria-label='outlined button group'>
           <Button
             onClick={handleCodeToEnClick}
-            variant={(queryMode === 'code-to-en') ? 'contained' : 'outlined'}
+            variant={queryMode === 'code-to-en' ? 'contained' : 'outlined'}
           >
             Code to English
           </Button>
           <Button
             onClick={handleEnToCodeClick}
-            variant={(queryMode === 'en-to-code') ? 'contained' : 'outlined'}
+            variant={queryMode === 'en-to-code' ? 'contained' : 'outlined'}
           >
             English to Code
           </Button>
           <Button
             onClick={handleEnToSQLClick}
-            variant={(queryMode === 'en-to-sql') ? 'contained' : 'outlined'}
+            variant={queryMode === 'en-to-sql' ? 'contained' : 'outlined'}
           >
             English to SQL
           </Button>
--- src/SearchedResults.js
+++ src/SearchedResults.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 //dropdown for history elements
 function SearchedResults(props) {
-  //searched is an array of objects returned from the DB, each object contains past search query info
+  //searched is an array of objects returned from the DB, each object contains past search query
   const { searched, handleElementClick } = props;
+  console.log('search history', props.searched);
   return (
     //renders history objects in state to components with two primary html subcomponents:
     //the javascript code sent by user and the english translation send back by API
@@ -15,13 +16,13 @@ function SearchedResults(props) {
           key={obj.translation}
         >
           <p>
-            <span>Code: </span>
-            {obj.code.slice(0, 100)}
+            <span>query: </span>
+            {obj.query.slice(0, 100)}
             ...
           </p>
           <p>
-            <span>Plain English: </span>
-            {obj.code.slice(0, 100)}
+            <span>Translation: </span>
+            {obj.translation.slice(0, 100)}
             ...
           </p>
         </div>
